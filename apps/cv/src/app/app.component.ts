797cea193a3566c76871c8fa26b17b90a951882a
Corentin Grosdemouge <kaeh@pop-os.localdomain>
2023-03-11 18:44:17 +0100

    Initial commit

---
+++ apps/cv/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'kaeh-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {}
