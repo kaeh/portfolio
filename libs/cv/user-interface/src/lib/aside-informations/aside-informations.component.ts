7a0f4567b92608030407f555233e1f663f5fafb6
Kaeh <corentin.grosdemouge@gmail.com>
2023-03-12 10:02:04 +0100

    add personal informations component to aside

--- libs/cv/user-interface/src/lib/aside-informations/aside-informations.component.ts
+++ libs/cv/user-interface/src/lib/aside-informations/aside-informations.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { CvPersonalInformationsComponent } from "./personal-informations/personal-informations.component";
 import { CvPhotoComponent } from "./photo/photo.component";
 
 @Component({
   selector: 'kaeh-aside-informations',
   standalone: true,
-  imports: [CvPhotoComponent],
+  imports: [CvPhotoComponent, CvPersonalInformationsComponent],
   templateUrl: './aside-informations.component.html',
   styleUrls: ['./aside-informations.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
